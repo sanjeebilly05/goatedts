3684f273d3e0a11f3b067c7c99d1aa2b10a849a1
sanjeebilly05 <Sanjeeb.Gurung@city.ac.uk>
2026-03-27 12:50:17 +0000

    made first commit

--- src/main/java/com/example/fx_testing/HelloApplication.java
+++ src/main/java/com/example/fx_testing/HelloApplication.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+//For commit pushes puroposes
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
